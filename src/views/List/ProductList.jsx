da88e9d158ebabe0a2464281765804ee5a951f06
artur <arturnowakowski2015@gmail.com>
2023-01-20 07:24:57 +0100

    fixed separate animation hook

--- src/views/List/ProductList.jsx
+++ src/views/List/ProductList.jsx
@@ -1,64 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { useView } from "./ViewModel";
+
+import { useAnimation } from "./AnimationModel";
 import List from "../../components/List";
 import Button from "../../components/Button";
 import { useNavigate } from "react-router-dom";
 
 export default function ProductList() {
   let navigate = useNavigate();
-  const { products, getProducts } = useView(0);
 
-  const [array, setArray] = useState([]);
-  let iteration = -1;
-  const func2 = (sWord) => {
-    iteration++;
-
-    if (iteration < (products && products.length)) {
-      setTimeout(() => {
-        func2(sWord);
-        setArray(
-          products &&
-            products.filter((t, i) => {
-              if (i < iteration) return t;
-            })
-        );
-
-        return array;
-      }, 1000);
-    }
-  };
+  const { array, animateLineOfProducts } = useAnimation();
   useEffect(() => {
-    getProducts();
+    animateLineOfProducts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  useEffect(() => {
-    func2("rrr");
-  }, [products]);
-  console.log(JSON.stringify(array));
-  /*
-  iteration = 0;
-  func1();
-  
-      function  func1() {
-          var result = func2("haha");
-          alert(iteration + ":" + result);
-      }
-  
-      function func2 (sWord) {
-          iteration++;
-  
-          sWord = sWord + "lol";
-          if ( iteration < 5 ) {
-          
-              func2( sWord);
-          } else {
-  
-              return sWord;
-          }
-  
-      return sWord;
-      }
-
-*/
 
   return (
     <div className="page">
