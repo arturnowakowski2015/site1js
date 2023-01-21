02b0af9d4330f93cf017690bf68e284c8b6d3479
artur <arturnowakowski2015@gmail.com>
2023-01-21 10:23:48 +0100

    fixed columns

--- src/Domain/UseCase/Product/UpdateProduct.js
+++
@@ -1,4 +0,0 @@
-import { updateProduct } from '../../../Data/Repository/ProductRepository'
-export async function UpdateProductUseCase(id, productData) {
-    return await updateProduct(id, productData)
-}
